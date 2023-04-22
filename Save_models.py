b943d2fb8a871387ec9cbf36abbf416b28646e38
yohoo <yohoo6353@gmail.com>
2023-04-22 22:48:22 +0800

    modified:   Create_Ensemble.py 	modified:   Save_models.py 	modified:   __pycache__/Create_Ensemble.cpython-39.pyc 	modified:   __pycache__/models.cpython-39.pyc 	new file:   jobmodels/secondlayer_clf.joblib 	new file:   jobmodels/the0th_firstlayer_clf.joblib 	new file:   jobmodels/the1th_firstlayer_clf.joblib 	new file:   jobmodels/the2th_firstlayer_clf.joblib 	new file:   jobmodels/the3th_firstlayer_clf.joblib

--- Save_models.py
+++ Save_models.py
@@ -1,8 +1,21 @@
 #%%
-import models
+from models import models
 from read_data import Read_data
-
+from  joblib import dump, load
 x_train, x_test, y_train, y_test, feature_names = Read_data.data()
 
 # %%
 clf = models(x_train, y_train, 2)
+firstlayer_models = clf.Stacking(x_test, return_firstlayer_models=True) #一级模型
+xtrain, xtest, secondlayer_model = clf.Stacking(x_test, return_first_labels=True) #二级模型
+# %%
+#将训练好的分类器保存到文件中
+j = 0
+for model in firstlayer_models:
+    dump(model, 'jobmodels\\the{}th_firstlayer_clf.joblib'.format(j))
+    test_predict = clf.Stacking(x_test) #二级模型输出结果标签
+    proba = clf.proba_value(x_test) #二级模型输出概率
+    j += 1
+
+dump(secondlayer_model, 'jobmodels\\secondlayer_clf.joblib')
+# %%
